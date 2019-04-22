6917d80c54721dd16add0705a7c69aea89863b75
THCloss <thiago_closs@hotmail.com>
2019-04-22 17:59:30 -0400

    Closs

--- app/src/main/java/com/hfad/monitorapet/MainActivity.java
+++ app/src/main/java/com/hfad/monitorapet/MainActivity.java
@@ -1,27 +1,49 @@
 package com.hfad.monitorapet;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static android.widget.Toast.*;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+          user = (EditText) findViewById(R.id.user);
+        Monitoria.Inicializar();
+        }
+
+        public void clicar (View v){
+
+        String valor=user.getText().toString();
+
+
+        if(Monitoria.validarMonitor(valor)){
+            Intent intent = new Intent(getApplicationContext(), listaP.class);
+            startActivity(intent);
+        }
+        else{
+
+            Toast.makeText(MainActivity. this,"Usuário não cadastrado!", LENGTH_LONG). show();
+
+
+        }
+
+
     }
+
 
 }
