5477dfb43df528b3b7d7bacd0d052e351167270b
Atwah <atwah98@gmail.com>
2019-04-30 13:07:29 -0300

    ATT

--- app/src/main/java/com/hfad/monitorapet/visitanteATV.java
+++ app/src/main/java/com/hfad/monitorapet/visitanteATV.java
@@ -6,24 +6,40 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class visitanteATV extends AppCompatActivity {
+    ListView visitante1;
+    ListView visitante2;
+    ListView visitante3;
+    ListView visitante4;
+
+    private String[] Lista1 = {"Credenciamento", "Cerimônia de Abertura", "Apresentação Cultural", "Palestra de Abertura", "Encontro de Discentes e Encontro de Docentes", " Livre (Bares, cinema)"};
+    private String[] Lista2 = {"Café da manhã", "Mesa Redonda", "Apresentação de Banner", "Almoço", "Encontros por Atividade / Encontro de Interlocutores e Membros do CLAA", " Intervalo", "Mobiliza PET", "Jantar","Livre (Bares, Cinema)"};
+    private String[] Lista3 = {"Café da manhã", "Mesa Redonda", "Apresentação Oral", "Almoço", "– Minicursos / Oficinas", " “Tchá com Bolo”", "Grupos de Discussão de Trabalho", "Jantar", "em diante – Festa Oficial – “Chuça e Rebuça”"};
+    private String[] Lista4 = {"Café da manhã", "Assembleia Final", "Almoço", "Assembleia Final", "Livre (Bares, cinema)"};
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_visitante_atv);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        visitante1 = (ListView) findViewById(R.id.list1);
+        ArrayAdapter<String> meuAdaptador = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, android.R.id.text1, Lista1);
+        visitante1.setAdapter(meuAdaptador);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        visitante2 = (ListView) findViewById(R.id.list2);
+        ArrayAdapter<String> meuAdaptador2 = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, android.R.id.text1, Lista2);
+        visitante2.setAdapter(meuAdaptador2);
+
+        visitante3 = (ListView) findViewById(R.id.list3);
+        ArrayAdapter<String> meuAdaptador3 = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, android.R.id.text1, Lista3);
+        visitante3.setAdapter(meuAdaptador3);
+
+        visitante4 = (ListView) findViewById(R.id.list4);
+        ArrayAdapter<String> meuAdaptador4 = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, android.R.id.text1, Lista4);
+        visitante4.setAdapter(meuAdaptador4);
     }
-
 }
--- app/src/main/java/com/hfad/monitorapet/visitanteMAP.java
+++ app/src/main/java/com/hfad/monitorapet/visitanteMAP.java
@@ -14,8 +14,8 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class visitanteMAP extends FragmentActivity implements OnMapReadyCallback {
 
-    double lat = -15.608868;
-    double longi = -56.065503;
+    double lat = -15.614597;
+    double longi = -56.070941;
     LatLng ll = new LatLng(lat, longi);
 
     private GoogleMap mMap;
@@ -46,15 +46,28 @@ public class visitanteMAP extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        mMap.addMarker(new MarkerOptions().position(new LatLng(lat,longi)).title("Praça do restaurante Universitário"));
+
+
+        //Lugares de atividades
+        mMap.addMarker(new MarkerOptions().position(new LatLng(lat,longi)).title("Credenciamento(FAMEVZ)"));
         mMap.addMarker(new MarkerOptions().position(new LatLng(-15.612655,-56.067532)).title("Assembléias e Mesas redondas"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(-15.613699, -56.069170)).title("Apresentações e Banners (ICHS)"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(-15.612553, -56.068925)).title("Encontro de Discentes e Docente"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(-15.608296, -56.066127)).title("Restaurante Universitário(Refeições)"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(-15.608269, -56.064817)).title("Auditório da FAET (Mofão)"));
+        //lUGARES AUXILIARES
+        mMap.addMarker(new MarkerOptions().position(new LatLng(-15.608809, -56.065735)).title("Praça do restaurante Universitário"));
         mMap.addMarker(new MarkerOptions().position(new LatLng(-15.612218, -56.073231)).title("Pegue um cineminha e faça compras"));
-        mMap.addMarker(new MarkerOptions().position(new LatLng(-15.612218, -56.073231)).title("Pegue um cinema e faça compras"));
         mMap.addMarker(new MarkerOptions().position(new LatLng(-15.593254, -56.104591)).title("Que tal dançar um pouco?"));
         mMap.addMarker(new MarkerOptions().position(new LatLng(-15.567847, -56.079925)).title("Um parque tranquilo com natureza e pistas de corrida"));
-        mMap.addMarker(new MarkerOptions().position(new LatLng(-15.612364, -56.066298)).title("30cm vai ser pouco pra você"));
-        mMap.addMarker(new MarkerOptions().position(new LatLng(-15.613699, -56.069170)).title("Apresentações e Banners (Em frente às quadras)"));
-        mMap.addMarker(new MarkerOptions().position(new LatLng(-15.614165, -56.073927)).title("Que tal um lanchinho"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(-15.613473, -56.068237)).title("Bar do Zapatta"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(-15.622497, -56.069597)).title("Taverna Corvo Negro"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(-15.605482, -56.073950)).title("Poltrona Nerd"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(-15.613086, -56.067620)).title("Espeto do João"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(-15.613140, -56.066954)).title("Natural Club"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(-15.607720, -56.072595)).title("Nossa Pizza Restaurante"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(-15.612364, -56.066298)).title("Subway"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(-15.614165, -56.073927)).title("Mc Donald’s"));
         mMap.addMarker(new MarkerOptions().position(new LatLng(-15.613157, -56.060742)).title("Recreio da UFMT(Bar do arcanjo)"));
 
         CameraUpdate location = CameraUpdateFactory.newLatLngZoom(ll, 15);
